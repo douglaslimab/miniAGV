4b455663214764f2a76c23c4fc4a94e50918a5e3
Douglas Lima Benevides <51888076+douglaslimab@users.noreply.github.com>
2021-01-13 20:15:46 +0000

    it needs to fix the app..

--- miniAGV/miniAGV.ino
+++ miniAGV/miniAGV.ino
@@ -1,18 +1,18 @@
 
 //---------------------------------------------------------------
-//Descrição: miniAGV 
-//Data: 30/08/2016 21:57        Última Atualização: 12/01/2021
-//Autor: Douglas Lima
-//Comentários:  Teste da Ponte H L298
-//              Teste com Módulo Bluetooth
-//              Teste com Suporte Pan/Tilt para Câmera
+//Project: miniAGV 
+//Started: 30/08/2016 21:57       Last Update: 13/01/2021
+//Author: Douglas Lima
+//Features:  Motor Shield L298P
+//           Bluetooth Module
+//           Pan/Tilt Servo Camera
 //---------------------------------------------------------------
 
 #include <Servo.h>
 
 Servo servo1, servo2;
-byte pos = 90;
-char comando[3];
+byte pos = 0;
+char comando[2];
 
 
 //Motor Shield Pins----------------------------------------------
@@ -26,15 +26,9 @@ const int sns0 = A0;
 const int sns1 = A1;
 
 const int speaker = 4;
- 
-int rele1 = 22;
-int rele2 = 23;
-int rele3 = 24;
-int rele4 = 25;
 
 void setup() {
   Serial.begin(9600);
-  Serial1.begin(9600);
 
   servo1.attach(9);
   servo2.attach(10);
@@ -52,29 +46,19 @@ void setup() {
 
   digitalWrite(brkA, LOW);
   digitalWrite(brkB, LOW);
-  
-  pinMode(rele1, OUTPUT);
-  pinMode(rele2, OUTPUT);
-  pinMode(rele3, OUTPUT);
-  pinMode(rele4, OUTPUT);
-  
-  digitalWrite(rele1, LOW);
-  digitalWrite(rele2, HIGH);
-  digitalWrite(rele3, HIGH);
-  digitalWrite(rele4, HIGH);
 }
 
 void loop() {
-  if (Serial1.available()) {
-    for (int i = 0; i < 3; i++) {
-      comando[i] = Serial1.read();
+  if (Serial.available()) {
+    for (int i = 0; i < 2; i++) {
+      comando[i] = Serial.read();
       delay(10);
     }
   }
-  pos = comando[0];
+  pos = comando[1];
 
-  switch (comando[1]) {
-    case 'a':
+  switch (comando[0]) {
+    case'a':
       servo1.write(pos);
       break;
     case 'b':
@@ -82,37 +66,39 @@ void loop() {
       break;
     case 'c':
       if (pos <= 127) {
-//        analogWrite(pwmA, 255 - pos*2);
-        analogWrite(pwmA, pos);
+        analogWrite(pwmA, 255 - pos*2);
         digitalWrite(dirA, HIGH);
-        Serial.print("Motor A: ");
-        Serial.println(pos);
       }
-      else if (pos > 127) {
+      else if (pos > 127) {  
         pos = pos - 127;
-//        analogWrite(pwmA, pos*2 - 1);
-        analogWrite(pwmA, pos);
-        digitalWrite(dirA, LOW);
-        Serial.print("Motor B: ");
-        Serial.println(pos);
+        analogWrite(pwmA, pos*2 - 1);
+        digitalWrite(dirA, LOW);      
       }
       break;
     case 'd':
       if (pos <= 127) {
-//        analogWrite(pwmB, 255 - pos*2);
-        analogWrite(pwmB, pos);
-        digitalWrite(dirB, HIGH);
-        Serial.print("Motor A: ");
-        Serial.println(pos);
+        analogWrite(pwmB, 255 - pos*2);
+        digitalWrite(dirB, LOW);
       }
       else if (pos > 127) {
         pos = pos - 127;
-//        analogWrite(pwmB, pos*2 - 1);
-        analogWrite(pwmB, pos);
-        digitalWrite(dirB, LOW);
-        Serial.print("Motor B: ");
-        Serial.println(pos);
+        analogWrite(pwmB, pos*2 - 1);
+        digitalWrite(dirB, HIGH);
       }
       break;
+      case 'e':
+      comando[0] = 'E';
+      buzz(3);
+      break;
+  }
+}
+
+void buzz(int f){
+  int i = 0;
+  for(i = 0; i < 20; i++){
+    digitalWrite(speaker, HIGH);
+    delay(f);
+    digitalWrite(speaker, LOW);
+    delay(f);
   }
 }
